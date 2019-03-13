db23fbc279b1786823ddb9571b6729717d4d0575
Marty Mazurik <marty@martymazurik.com>
2019-03-13 14:35:12 -0700

    working to POST to RabbitMQ

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/streadway/amqp"
@@ -19,19 +19,13 @@ func main() {
 
 // Person : is for person
 type Person struct {
-	ID        string   `json:"id,omitempty"`
-	Firstname string   `json:"firstname,omitempty"`
-	Lastname  string   `json:"lastname,omitempty"`
-	Address   *Address `json:"address,omitempty"`
+	ID        string `json:"id,omitempty"`
+	Firstname string `json:"firstname,omitempty"`
+	Lastname  string `json:"lastname,omitempty"`
+	City      string `json:"city,omitempty"`
+	State     string `json:"state,omitempty"`
 }
 
-// Address : is for addresses
-type Address struct {
-	City  string `json:"city,omitempty"`
-	State string `json:"state,omitempty"`
-}
-
-// Create person, publish to topic
 func CreatePerson(w http.ResponseWriter, r *http.Request) {
 	//params := mux.Vars(r)
 	var person Person
@@ -40,26 +34,56 @@ func CreatePerson(w http.ResponseWriter, r *http.Request) {
 
 	//url := os.Getenv("CLOUDAMQP_URL")
 	url := "amqp://iipsuwdh:d6SvD6ME8MyYbSbDwCeFDOEEb7rSLEgK@otter.rmq.cloudamqp.com/iipsuwdh"
-	connection, _ := amqp.Dial(url)
-	//	if err != nil {
-	//		return fmt.Errorf("Dial: %s", err)
-	//	}
+	conn, err := amqp.Dial(url)
+	if err != nil {
+		fmt.Println("err: ", err)
+		//		return fmt.Errorf("Dial: %s", err)
+	}
+	defer conn.Close()
 
-	go func(con *amqp.Connection) {
-		timer := time.NewTicker(1 * time.Second)
-		channel, _ := connection.Channel()
+	ch, _ := conn.Channel()
+	defer ch.Close()
 
-		for t := range timer.C {
-			msg := amqp.Publishing{
-				DeliveryMode: 1,
-				Timestamp:    t,
-				ContentType:  "text/plain",
-				Body:         []byte("Hello world"),
-			}
-			mandatory, immediate := false, false
-			channel.Publish("amq.topic", "ping", mandatory, immediate, msg)
-		}
-	}(connection)
+	queue, err := ch.QueueDeclare(
+		"personQueue",
+		false, //persist
+		false, //autodelete,
+		false, //exclusive
+		false, //no-wait
+		nil,   // adv args
+	)
 
-	select {}
+	var body []byte
+	body, err = json.Marshal(person) // convert to []byte array
+	if err != nil {
+		fmt.Println("err:", err)
+	}
+	err = ch.Publish(
+		"",         //exchange
+		queue.Name, //routing key
+		false,      //mandatory
+		false,      //immediate
+		amqp.Publishing{
+			ContentType: "text/plain", // MIME content type https://www.freeformatter.com/mime-types-list.html
+			Body:        body,
+		})
+
+	// goroutine asynch?
+	// go func(con *amqp.Connection) {
+	// 	timer := time.NewTicker(1 * time.Second)
+	// 	channel, _ := connection.Channel()
+
+	// 	for t := range timer.C {
+	// 		msg := amqp.Publishing{
+	// 			DeliveryMode: 1,
+	// 			Timestamp:    t,
+	// 			ContentType:  "text/plain",
+	// 			Body:         []byte("Hello world"),
+	// 		}
+	// 		mandatory, immediate := false, false
+	// 		channel.Publish("amq.topic", "ping", mandatory, immediate, msg)
+	// 	}
+	//}(connection)
+
+	// select {}
 }
